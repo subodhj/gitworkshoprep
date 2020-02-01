6ecdfdf9cfd6fbbeaa58c9e8ae892597c6fa3054
HanumanDev2 <hanuman@gmail.com>
2020-02-01 16:13:19 +0530

    Printing  1 to 10 digits using for loop

--- GitProj/src/com/nt/test/SampleApp.java
+++ GitProj/src/com/nt/test/SampleApp.java
@@ -4,7 +4,10 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
 
-	}
+	}// main
 
-}
+}// class
